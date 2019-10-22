6e6438aadaa0db22a6a4978931a38edd3246cff8
Theo Armour <theo-armour@users.noreply.github.com>
2019-10-21 21:45:44 -0700

    update links

---
+++ demo/paul-m/math-cell/calabi-yau/checkMathML.js
@@ -0,0 +1,9 @@
+
+if ( typeof InstallTrigger === 'undefined' ) {
+
+	var script = document.createElement( 'script' );
+	script.src = 'https://cdn.jsdelivr.net/gh/mathjax/MathJax@2.7.1/MathJax.js?config=MML_HTMLorMML';
+	document.head.appendChild( script );
+
+}
+
