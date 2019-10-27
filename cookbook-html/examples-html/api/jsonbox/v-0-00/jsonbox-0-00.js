9353a2e7875144b73a503926b323c107b3977346
Theo Armour <theo-armour@users.noreply.github.com>
2019-10-27 14:58:55 -0700

    jsonbox

---
+++ cookbook-html/examples-html/api/jsonbox/v-0-00/jsonbox-0-00.js
@@ -0,0 +1,87 @@
+"use strict"
+
+const JB = {}
+
+JB.url = "https://jsonbox.io/box_34acd03fcbcfd2fa1051"
+
+
+window.addEventListener( 'focus', () => JB.getJson() );
+
+
+JB.getJson = function () {
+
+	if ( !JB.interval ) { JB.interval = setInterval( JB.putJson, 2000 ); }
+
+	window.onunload = JB.putJson( "" );
+
+	const request = new Request( JB.url + "/5db548fe8d6b3900173a5721" )
+	fetch( request )
+	.then( response => response.json() )
+	.then( data => {
+		JB.json = data;
+		JB.contentPrevious = divContent.innerHTML = JB.json.content;
+		console.log( 'JB.json', JB.json);
+
+	} );
+
+};
+
+
+JB.putJson = function( previous = JB.contentPrevious ) {
+
+	const content = divContent.innerHTML;
+
+	if ( content === previous ) { return; }
+
+	JB.contentPrevious = content;
+
+	fetch( JB.url + "/5db548fe8d6b3900173a5721", {
+		method: "PUT",
+		headers: { "content-type": "application/json" },
+		body: JSON.stringify( { "content": content, } ),
+
+	} )
+	.then( response => response.json() )
+	.then( data => {
+
+		divResponse.innerHTML = `<p>${ new Date().toLocaleTimeString() }</p>${ content }`;
+		//divResponse.innerHTML = JSON.stringify( data );
+
+	} )
+
+}
+
+
+
+
+JB.getJsonAll = function () {
+
+	const request = new Request( JB.url )
+	fetch( request )
+		.then( response => response.json() )
+		.then( data => {
+			JB.json = data;
+			console.log( 'JB.json', JB.json );
+			//divContent.innerHTML = JB.json;
+		} );
+
+};
+
+
+JB.deleteJson = function ( index = 1 ) {
+
+	id = JB.json[ index ]._id;
+
+	fetch( JB.url + "/" + id, {
+		method: "DELETE",
+		headers: { "content-type": "application/json" }
+
+	} )
+		.then( response => response.json() )
+		.then( data => {
+
+			divResponse.innerText += `${ JSON.stringify( data ) }\n\n`;
+
+		} )
+
+};
